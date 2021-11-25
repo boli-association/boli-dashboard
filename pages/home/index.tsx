0a6ca1614a374fba98abd6cbc4279136300d48d4
sudddy <suddpro@gmail.com>
2021-11-26 10:58:22 +1300

    removed unused lines.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    console.log("test");
     return (
         <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-    Welcome to boli  money home!
+      Welcome to boli money home!
         </div>
     );
 };
