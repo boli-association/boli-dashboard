df3fbc77669ef3de5a2779332beb23b99fae2a70
sudddy <suddpro@gmail.com>
2021-11-25 16:31:38 +1300

    Setting up initial pages

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
   <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-    Welcome to boli money!
+    Welcome to boli  money home!
   </div>
   )
 }
