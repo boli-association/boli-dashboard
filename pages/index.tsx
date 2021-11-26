4588a81a2b5937dcb493f4e7be169158f1226b8a
sudddy <suddpro@gmail.com>
2021-11-27 10:33:14 +1300

    fixed formatting stuffs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-  <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
-    Welcome to boli money!
-  </div>
-  )
-}
+    return (
+        <div className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">
+      Welcome to boli money!
+        </div>
+    );
+};
 
-export default Home
+export default Home;
