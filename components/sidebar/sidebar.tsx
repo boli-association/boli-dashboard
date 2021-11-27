8a456f9f6f75b13ee5947555fa73840614ff38ed
sudddy <suddpro@gmail.com>
2021-11-27 14:37:35 +1300

    Added sidebar component

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,9 +1,55 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { FunctionComponent } from "react";
+import Image from "next/image";
+import TickBox from "../../public/assets/images/checkbox-circle-fill.svg";
 
 const Sidebar:FunctionComponent = () => {
     return (
-        <div className="bg-green-300 border-green-600">
-         This is the sidebar
+        <div className=" px-5">
+            <div className="py-10">
+
+            </div>
+            <div className="flex  flex-col items-center py-5 py-6 hover:red">
+                <Image alt="tick" src={TickBox} className="w-full h-9 hover:red " />
+                <a href="/" className="text-primary pt-4 text-10 hover:red ">
+                 Dashboard
+                </a>
+
+            </div>
+            <div className="flex flex-col items-center  py-5 py-6">
+                <Image alt="tick" src={TickBox} className="w-full h-9" />
+                <a href="/" className="text-primary pt-4 text-10 ">
+                 Wallet
+                </a>
+
+            </div>
+            <div className="flex flex-col items-center  py-5 py-6 ">
+                <Image alt="tick" src={TickBox} className="w-full h-9" />
+                <a href="/" className="text-primary pt-4 text-10">
+                 Swap
+                </a>
+
+            </div>
+            <div className="flex flex-col items-center py-5 py-6 ">
+                <Image alt="tick" src={TickBox} className="w-full h-9" />
+                <a href="/" className="text-primary pt-4 text-10">
+                 Proposals
+                </a>
+
+            </div>
+            <div className="flex flex-col items-center py-5 py-6 ">
+                <Image alt="tick" src={TickBox} className="w-full h-9" />
+                <a href="/" className="text-primary pt-4 text-10">
+                 Staking
+                </a>
+
+            </div>
+            <div className="flex flex-col items-center py-5 py-6 ">
+                <Image alt="tick" src={TickBox} className="w-full h-9" />
+                <a href="/" className="text-primary pt-4 text-10">
+                 History
+                </a>
+            </div>
         </div>
     );
 
