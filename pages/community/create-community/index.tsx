ecf394b6ce10c74d21766945419b04d0673b27de
sudddy <suddpro@gmail.com>
2021-12-07 17:31:13 +1300

    routing and events

--- pages/community/create-community/index.tsx
+++ pages/community/create-community/index.tsx
@@ -5,9 +5,10 @@ import ExploreProject from "@/components/cards/explore-project/ExploreProject";
 import TrendingCommunity from "@/components/cards/trending-community/TrendingCommunity";
 import CreateCommunity from "@/components/cards/create-community/CreateCommunity";
 import CreateToken from "@/components/cards/create-token/CreateToken";
+import { Router, useRouter } from "next/dist/client/router";
 
 const Community: FunctionComponent = () => {
-  const community = ['Nike', 'Eco Org', 'R'];
+  
   return (
     <Layout>
       <div className="flex-1 min-w-full">
